fbc653561f603c47c6c1b871ebd5182a52796d15
Rémi Dewame <remi.dewame.etu@univ-lille.fr>
2023-10-10 14:11:55 +0200

    Correctif

--- src/components/pages/home/Home.vue
+++ src/components/pages/home/Home.vue
@@ -1,15 +1,16 @@
 <script setup>
 //import Timeline from "@/components/pages/home/components/";
 import WaveDivider from "@/components/header/WaveHeader.vue";
+import Timeline from "@/components/pages/roadmap/components/timeline/Timeline.vue";
 </script>
 
 <template>
-    <div>
-        <WaveDivider title="DÉCOUVREZ LE PROJET/>
-        <div style="display: flex" class="align-center justify-center">
-            <Timeline/>
-        </div>
+  <div>
+    <WaveDivider title="DÉCOUVREZ LE PROJET"/>
+    <div style="display: flex" class="align-center justify-center">
+      <Timeline/>
     </div>
+  </div>
 </template>
 
 <style>
